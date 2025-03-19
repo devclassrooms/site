b4821605205b26fdd5ca93d8e4f236e12dab3138
devclassrooms <bh0@laposte.net>
2025-03-19 02:52:47 +0100

    chargement

--- site/src/App.js
+++ site/src/App.js
@@ -27,87 +27,90 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div className={isBlack ? 'black-bg container' : 'white-bg container'}> 
-        <header className="App-header">
-          <nav>
-            <div className='img_logo'>
-              <NavLink to="/">
-                <img src={Img} alt="image" />
-              </NavLink>
-            </div>
-            <div className='lien' id='lien_id'>
-              <div className='container_a'>
-              <NavLink className='lienAccueil' to="/" id='vvv'>Accueil</NavLink>
-              </div>
-              <div className='container_a'>
-              <NavLink className='lienContacte'to="/apropos" id='vvv'>Contacte</NavLink>
-              </div>
-              <div className='container_a'>
-              <NavLink className='lienProjet'to="/projet" id='vvv'>Projet</NavLink>
-              </div>
-            </div>
-            <div className='img_logo_dev'>
-              <img 
-                className={isBlack ? 'black-bg img_dev_nav' : 'white-bg img_dev_nav'}
-                src="https://media1.giphy.com/media/WIAXKEDP8R0IBSAXUk/giphy.gif" // Remplace par ton image
-                alt="Toggle Background" 
-                onClick={toggleBackground} 
-                />
-             </div>
-          </nav>
-          <div className='num_tel'>
-            <a className='bt_num' href="tel:+33758125523"><img className='svg_tel' src={Img_ordi2} alt="Appeler ce numéro" width="30" height="30"/>07.58.12.55.23</a>
+        <div id='chargement'>
+          <img src="https://i.pinimg.com/originals/d7/ac/8f/d7ac8fe75a93307683db992d9c8c6f8c.gif"></img>
           </div>
-        </header>
-        <div className='img_mid'>
-          <div className='img_text'>
-            <p>Bienvenue sur mon site ! Je suis heureux de vous accueillir.</p>
-            <img src={Img_ordi}>
-            </img>
-          </div>
-        </div>
-        <div className='mid'>
-            <div>
-              <p><span className='mid_name'>Je m’appelle Hosni Bouhlel Boughdira </span><br></br> Je suis <span className='text_l'>développeur web</span>. J’ai acquis une solide expertise dans différents langages de programmation tels que <span className='text_l'>C, C++, JavaScript, Node.js et React.js,</span> ainsi que dans les <span className='text_l'>API REST</span> et le référencement naturel <span className='text_l'>(SEO)</span>.</p>
-            </div>
-            <div className='telecharger'>
-                <a href="https://devclassrooms.github.io/site/cv_pro.pdf" download="cv_pro.pdf">
-                  <span>Télécharger</span>
-                  <span>CV</span> 
-                </a>
-            </div>
-            <div>
-              <p>J’ai suivi une formation de deux ans à l’école 42, reconnue pour sa rigueur et son excellence en matière de développement informatique. J’ai également suivi des cours en ligne sur la plateforme Openclassrooms, où j’ai obtenu un diplôme de développeur web de niveau bac+2.</p>
-            </div>
-            <div>
-              <p>Je suis actuellement à la recherche d’opportunités pour travailler dans le domaine du développement web, que ce soit en tant que développeur en entreprise ou en freelance. J’ai une grande passion pour la création de sites web et je suis prêt à relever tous les défis qui me seront proposés.</p>
-            </div>
-            <div>
-              <p>N’hésitez pas à parcourir mon portfolio pour découvrir mes projets passés et à me contacter si vous avez des questions ou si vous souhaitez travailler avec moi sur un projet de développement web. Je suis impatient de collaborer avec vous !</p>
-            </div>
-            <div className={isHide ? 'warning_togle_hide' : 'warning_togle'}>
-              <div className='warning_mid'>
-                <h3>Attention le site est actuellement en maintenance. Certaines fonctionnalités pourraient être temporairement inaccessibles. Merci de votre compréhension.</h3>
-                <span onClick={toggleHide}>x</span>
-              </div>
-            </div>
-        </div>
-        <div className='footer'>
-          <footer>
-            <div className='footer_img_logo' id='footer_img_logo_id'>
-                <NavLink to="/">
-                  <img className='img_footer' src={Img} alt="image"/>
+          <div className={isBlack ? 'black-bg container' : 'white-bg container'}> 
+          <header className="App-header">
+            <nav>
+              <div className='img_logo'>
+                <NavLink to="/site">
+                  <img src={Img} alt="image" />
                 </NavLink>
+              </div>
+              <div className='lien' id='lien_id'>
+                <div className='container_a'>
+                <NavLink className='lienAccueil' to="/site" id='vvv'>Accueil</NavLink>
+                </div>
+                <div className='container_a'>
+                <NavLink className='lienContacte'to="/apropos" id='vvv'>Contacte</NavLink>
+                </div>
+                <div className='container_a'>
+                <NavLink className='lienProjet'to="/projet" id='vvv'>Projet</NavLink>
+                </div>
+              </div>
+              <div className='img_logo_dev'>
+                <img 
+                  className={isBlack ? 'black-bg img_dev_nav' : 'white-bg img_dev_nav'}
+                  src="https://media1.giphy.com/media/WIAXKEDP8R0IBSAXUk/giphy.gif" // Remplace par ton image
+                  alt="Toggle Background" 
+                  onClick={toggleBackground} 
+                  />
+              </div>
+            </nav>
+            <div className='num_tel'>
+              <a className='bt_num' href="tel:+33758125523"><img className='svg_tel' src={Img_ordi2} alt="Appeler ce numéro" width="30" height="30"/>07.58.12.55.23</a>
             </div>
-            <div>
-                <p>© 2023 Hosni. All rights reserved</p>
+          </header>
+          <div className='img_mid'>
+            <div className='img_text'>
+              <p>Bienvenue sur mon site ! Je suis heureux de vous accueillir.</p>
+              <img src={Img_ordi}>
+              </img>
             </div>
-          </footer>
-        </div>
-        <Routes>
-            <Route exact path="/" component={Home} />
-        </Routes>
-        </div>
+          </div>
+          <div className='mid'>
+              <div>
+                <p><span className='mid_name'>Je m’appelle Hosni Bouhlel Boughdira </span><br></br> Je suis <span className='text_l'>développeur web</span>. J’ai acquis une solide expertise dans différents langages de programmation tels que <span className='text_l'>C, C++, JavaScript, Node.js et React.js,</span> ainsi que dans les <span className='text_l'>API REST</span> et le référencement naturel <span className='text_l'>(SEO)</span>.</p>
+              </div>
+              <div className='telecharger'>
+                  <a href="https://devclassrooms.github.io/site/cv_pro.pdf" download="cv_pro.pdf">
+                    <span>Télécharger</span>
+                    <span>CV</span> 
+                  </a>
+              </div>
+              <div>
+                <p>J’ai suivi une formation de deux ans à l’école 42, reconnue pour sa rigueur et son excellence en matière de développement informatique. J’ai également suivi des cours en ligne sur la plateforme Openclassrooms, où j’ai obtenu un diplôme de développeur web de niveau bac+2.</p>
+              </div>
+              <div>
+                <p>Je suis actuellement à la recherche d’opportunités pour travailler dans le domaine du développement web, que ce soit en tant que développeur en entreprise ou en freelance. J’ai une grande passion pour la création de sites web et je suis prêt à relever tous les défis qui me seront proposés.</p>
+              </div>
+              <div>
+                <p>N’hésitez pas à parcourir mon portfolio pour découvrir mes projets passés et à me contacter si vous avez des questions ou si vous souhaitez travailler avec moi sur un projet de développement web. Je suis impatient de collaborer avec vous !</p>
+              </div>
+              <div className={isHide ? 'warning_togle_hide' : 'warning_togle'}>
+                <div className='warning_mid'>
+                  <h3>Attention le site est actuellement en maintenance. Certaines fonctionnalités pourraient être temporairement inaccessibles. Merci de votre compréhension.</h3>
+                  <span onClick={toggleHide}>x</span>
+                </div>
+              </div>
+          </div>
+          <div className='footer'>
+            <footer>
+              <div className='footer_img_logo' id='footer_img_logo_id'>
+                  <NavLink to="/site">
+                    <img className='img_footer' src={Img} alt="image"/>
+                  </NavLink>
+              </div>
+              <div>
+                  <p>© 2023 Hosni. All rights reserved</p>
+              </div>
+            </footer>
+          </div>
+          <Routes>
+              <Route exact path="/" component={Home} />
+          </Routes>
+          </div>
       </div>
     </Router>
   );
